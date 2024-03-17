ea1d3c277699fefc18c081d58123f972aee24f31
pankaj <pankajbr9576@gmail.com>
2024-03-17 18:51:29 +0530

    Addition program

--- Session-4/Expriment.py
+++ Session-4/Expriment.py
@@ -1,7 +1,12 @@
 def Addition(n1,n2):
+    '''MY name is pankaj'''
     n3=int(n1)+int(n2)
     print(n3)
-
+print(Addition.__doc__)
 n1=input("Enter The no1=")
 n2=input("Enter The no2=")
 Addition(n1,n2)
+de=7//4
+de2=n1 % n2
+print(de2)
+print(de)
---
+++ Session5.py
@@ -0,0 +1,8 @@
+str="HelloDosto"
+# print(str)
+# print(str.upper())
+# print(str.lower())
+# print(str)
+list=['pankaj','Kumar,singh']
+list=str.split("o")
+print(list)
